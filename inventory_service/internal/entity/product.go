31f6b639df6e215fa2aa54ce244225fd1e075348
rrxshxd <gajiev.r.05@gmail.com>
2025-04-08 20:13:33 +0500

    Created product entity

---
+++ inventory_service/internal/entity/product.go
@@ -0,0 +1,14 @@
+package entity
+
+import "time"
+
+type Product struct {
+	ID          uint      `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Category    string    `json:"category"`
+	Price       float64   `json:"price"`
+	Stock       int       `json:"stock"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
