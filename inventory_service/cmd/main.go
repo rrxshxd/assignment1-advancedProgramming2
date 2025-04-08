b49e65d0075911df0ecc447fdbc7d3e3110f2078
rrxshxd <gajiev.r.05@gmail.com>
2025-04-09 00:07:43 +0500

    Minor fixes in config and created main file for products microservice

--- inventory_service/cmd/main.go
+++ inventory_service/cmd/main.go
@@ -1,1 +1,38 @@
-package cmd
+package main
+
+import (
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"github.com/rrxshxd/assignment1_advProg2/inventory_service/internal/config"
+	"github.com/rrxshxd/assignment1_advProg2/inventory_service/internal/controller"
+	"github.com/rrxshxd/assignment1_advProg2/inventory_service/internal/repository/postgres"
+	"github.com/rrxshxd/assignment1_advProg2/inventory_service/internal/usecase"
+	"log"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+
+	db, err := sql.Open("postgres", cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	productRepo := postgres.NewProductRepository(db)
+	productUseCase := usecase.NewProductUseCase(productRepo)
+	inventoryController := controller.NewInventoryController(productUseCase)
+
+	router := gin.Default()
+
+	router.POST("/products/create", inventoryController.CreateProduct)
+	router.GET("/products/:id", inventoryController.GetProduct)
+	router.PATCH("/products/:id", inventoryController.UpdateProduct)
+	router.DELETE("/products/:id", inventoryController.DeleteProduct)
+	router.GET("/products", inventoryController.GetAll)
+
+	if err := router.Run(":" + cfg.Port); err != nil {
+		log.Fatal(err)
+	}
+}
--- inventory_service/internal/config/config.go
+++ inventory_service/internal/config/config.go
@@ -1,1 +1,24 @@
 package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	Port        string
+	DatabaseURL string
+}
+
+func LoadConfig() *Config {
+	return &Config{
+		Port:        getEnv("PORT", "8081"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:Roshik8956@localhost:5432/products?sslmode=disable"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
