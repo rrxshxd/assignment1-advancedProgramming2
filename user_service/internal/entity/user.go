59ff70558920abd9b8063810e2f4bb85eed07673
rrxshxd <gajiev.r.05@gmail.com>
2025-04-15 17:38:33 +0500

    Created user entity for user service

---
+++ user_service/internal/entity/user.go
@@ -0,0 +1,25 @@
+package entity
+
+import "time"
+
+type User struct {
+	ID        uint      `json:"id"`
+	Email     string    `json:"email"`
+	Username  string    `json:"username"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type Address struct {
+	ID        uint      `json:"id"`
+	UserID    uint      `json:"user_id"`
+	Street    string    `json:"street"`
+	City      string    `json:"city"`
+	State     string    `json:"state"`
+	Zip       string    `json:"zip"`
+	Country   string    `json:"country"`
+	IsDefault bool      `json:"is_default"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
